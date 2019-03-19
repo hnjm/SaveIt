80998214fde613d2eb29971b4cccacb3e003e65a
sofia819 <sofia819@gmail.com>
2019-03-19 17:25:07 -0400

    Testing Pie Chart
    Testing chart customizations.

--- app/src/main/java/saveit/com/saveit/MainActivity.java
+++ app/src/main/java/saveit/com/saveit/MainActivity.java
@@ -8,6 +8,7 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private PieChart chart;
+    private float ftSize = 22f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +28,30 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of using pie chart from MPAndroidChart
         List<PieEntry> pieVal = new ArrayList<>();          // make a list of values in the chart and labels
-        pieVal.add(new PieEntry(1f, "red"));
-        pieVal.add(new PieEntry(2f, "green"));
-        pieVal.add(new PieEntry(3f, "blue"));
+        pieVal.add(new PieEntry(1f, "one"));
+        pieVal.add(new PieEntry(2f, "two"));
+        pieVal.add(new PieEntry(3f, "three"));
 
         PieDataSet pieDataSet = new PieDataSet(pieVal, "RGB");  // set data and name to pieDataSet
 
-        int[] color = {Color.RED, Color.GREEN, Color.BLUE};     // making an int[] for the colors
+        int[] color = {Color.RED, Color.BLUE, Color.DKGRAY};     // making an int[] for the colors
         pieDataSet.setColors(color);    // setting colors for the pie chart
 
         PieData pieData = new PieData(pieDataSet);
+        pieData.setValueTextSize(ftSize);   // set % value font size
+        pieData.setValueTextColor(Color.WHITE); // set value font color
+        pieDataSet.setValueFormatter(new PercentFormatter());   // use % formatting
+
         chart.setData(pieData); // set data to pie chart
+        chart.setUsePercentValues(true);    // use %
+        chart.setEntryLabelTextSize(ftSize); // set pie chart description font size
+        chart.getDescription().setEnabled(false);   // remove description next to chart
+        chart.getLegend().setEnabled(false);        // remove legend
+        chart.setHoleRadius(30f);   // radius of the empty space in the middle of the chart
+        chart.setTransparentCircleAlpha(0); // makes center circle transparent
+        //chart.setCenterText("Summary"); // set text in the middle of the chart
+        //chart.setCenterTextSize(ftSize); // set size of center text
+
         chart.invalidate();     // refresh
 
     }
